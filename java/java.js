f7228e442888566ca9d8db03b991a1bf83484f84
SaifJouda <saifjouda7@gmail.com>
2024-04-25 00:23:50 -0400

    tech section start

--- java/java.js
+++ java/java.js
@@ -9,3 +9,33 @@ window.addEventListener("mousemove", updateCursorPosition);
 //document.body.addEventListener("scroll", updateCursorPosition);
 
 
+function createTechnology(iconSrc, labelText) {
+  var technologyDiv = document.createElement('div');
+  technologyDiv.classList.add('technology');
+
+
+  //var roundDiv = document.createElement('div');
+  //roundDiv.classList.add('round');
+  
+  var iconDiv = document.createElement('div');
+  iconDiv.classList.add('icon');
+  var iconImg = document.createElement('img');
+  iconImg.src = iconSrc; // Set the source of the image
+  iconImg.alt = labelText; // Set the alt text for accessibility
+  iconDiv.appendChild(iconImg);
+  
+  var labelDiv = document.createElement('div');
+  labelDiv.classList.add('label');
+  labelDiv.textContent = labelText;
+  
+  technologyDiv.appendChild(iconDiv);
+  technologyDiv.appendChild(labelDiv);
+  
+  //roundDiv.appendChild(technologyDiv);
+
+  document.querySelector('.technology-section').appendChild(technologyDiv);
+}
+
+// Usage example:
+createTechnology('images\icons\c-sharp.png', 'Technology 1');
+createTechnology('images\icons\c-sharp.png', 'Technology 2');
